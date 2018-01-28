2eb8e3051625190d5cae6bdf612d9224ec952f88
Sam Gladstone <gladston@umich.edu>
2018-01-27 21:27:11 -0700

    Add Friend or Foe

---
+++ C#/friend-or-foe.cs
@@ -0,0 +1,19 @@
+/*
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+Note: keep the original order of the names in the output.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata {
+  public static IEnumerable<string> FriendOrFoe (string[] names) {
+    const int FRIENDLENGTH = 4;
+    return from name in names where name.Length == FRIENDLENGTH select name;
+  }
+}
